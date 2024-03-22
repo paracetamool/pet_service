57d7e37a236b1287b43365cd91d879a6123aa254
paracetamool <brom.1997@mail.ru>
2024-03-23 01:12:25 +0300

    add logging and first prefetch

--- service/services/views.py
+++ service/services/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from .models import Subscription
+from clients.models import Client
 from .serializers import SubscriprionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only(
+                                                            "company_name",
+                                                            "user__email")))
     serializer_class = SubscriprionSerializer
