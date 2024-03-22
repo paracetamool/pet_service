787b6cb6c2c38cb36edd4c8d62e8d67d56ff4d72
paracetamool <brom.1997@mail.ru>
2024-03-23 00:57:59 +0300

    add api/subscriptions

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import Subscription
+from .serializers import SubscriprionSerializer
 
-# Create your views here.
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriprionSerializer
