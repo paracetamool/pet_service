add218c387ecbea87de5aa91f76b5558952d2483
paracetamool <brom.1997@mail.ru>
2024-03-23 13:29:44 +0300

    add task for celery

--- service/services/models.py
+++ service/services/models.py
@@ -1,10 +1,24 @@
+from typing import Any, Iterable
 from django.db import models
 from django.core.validators import MaxValueValidator
 from clients.models import Client
+from .tasks import set_price
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs) -> None:
+
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
 
 class Plan(models.Model):
@@ -18,7 +32,21 @@ class Plan(models.Model):
     discount_persent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
 
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.__discount_persent = self.discount_persent
+
+    def save(self, *args, **kwargs) -> None:
+
+        if self.discount_persent != self.__discount_persent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
+
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
--- service/services/views.py
+++ service/services/views.py
@@ -13,7 +13,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
         Prefetch('client', queryset=Client.objects.all().select_related('user').only(
                                                             "company_name",
                                                             "user__email"))
-        ).annotate(price=F('service__full_price')-F('service__full_price')*F('plan__discount_persent')/100.00)
+        )
     serializer_class = SubscriprionSerializer
 
     def list(self, request, *args, **kwargs):
